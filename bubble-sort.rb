2b5ca337e6d1c489efa4b52892c6fed0d95df9d6
peuchen <npeuchen@gmail.com>
2020-09-30 22:30:54 +0200

    Add options to repeat loop

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -2,19 +2,27 @@
 def bubble_sort(numbers)
 #Compare pairs of numbers
 #If the first number is higher than the second, switch them around
+  output = []
   a = 0
-  while (a+1) < numbers.length
-    if numbers[a] > numbers[a+1]
-      old_num = numbers[a]
-      numbers[a] = numbers[a+1]
-      numbers[a+1] = old_num
+  if numbers.is_a?(Array)
+    while (a+1) < numbers.length
+      if numbers[a] > numbers[a+1]
+        old_num = numbers[a]
+        numbers[a] = numbers[a+1]
+        numbers[a+1] = old_num
+      end
+    #Else, keep the numbers in the same order
+    #Compare the next pairs
+    #Return the sorted array
+      a += 1
     end
-  #Else, keep the numbers in the same order
-  #Compare the next pairs
-  #Return the sorted array
-    a += 1
   end
-  numbers
+  output.unshift(numbers[-1])
+  numbers.tap(&:pop)
+  if numbers.length > 1
+    bubble_sort(numbers)
+  end
+  output
 end
 
 p bubble_sort([4,3,78,2,0,2])
