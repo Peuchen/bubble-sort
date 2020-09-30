77f3bfe4d06d909c9e2abb5c16ca3eed30c62593
peuchen <npeuchen@gmail.com>
2020-09-30 21:36:15 +0200

    Initiated bubble-sort.rb using pseudocode

---
+++ bubble-sort.rb
@@ -0,0 +1,6 @@
+#Define a method that accepts an array as an argument
+#Compare pairs of numbers
+#If the first number is higher than the second, switch them around
+#Else, keep the numbers in the same order
+#Compare the next pairs
+#Return the sorted array
