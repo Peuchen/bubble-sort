fee1bde9864d0a3f176a38f289136428a563858c
peuchen <npeuchen@gmail.com>
2020-09-30 21:54:07 +0200

    Complete first comparisons

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,6 +1,20 @@
 #Define a method that accepts an array as an argument
+def bubble_sort(numbers)
 #Compare pairs of numbers
 #If the first number is higher than the second, switch them around
-#Else, keep the numbers in the same order
-#Compare the next pairs
-#Return the sorted array
+  a = 0
+  while (a+1) < numbers.length
+    if numbers[a] > numbers[a+1]
+      old_num = numbers[a]
+      numbers[a] = numbers[a+1]
+      numbers[a+1] = old_num
+    end
+  #Else, keep the numbers in the same order
+  #Compare the next pairs
+  #Return the sorted array
+    a += 1
+  end
+  numbers
+end
+
+p bubble_sort([4,3,78,2,0,2])
